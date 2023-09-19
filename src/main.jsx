0e2e3dd30e1e8953c810fbec0a3f392cfd491a57
joujonikiasaroy <joujonikiasaroy@gmail.com>
2023-09-19 16:38:11 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello. you are in home age</div>,
+  },
+  {
+    path: "/about",
+    element: <div>Hello, you are in about page</div>,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
